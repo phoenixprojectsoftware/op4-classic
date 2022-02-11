fcb4fafd9272cfb998591a4b8831703ecf34314d
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2022-02-11 12:07:27 +0000

    Update mp5.cpp

--- dlls/mp5.cpp
+++ dlls/mp5.cpp
@@ -127,7 +127,7 @@ bool CMP5::Deploy()
 void CMP5::PrimaryAttack()
 {
 #ifndef CLIENT_DLL
-	int mp5_old_rate = sv_mp5_old_rate.value;
+	int sv_mp5_old_rate = mp5_old_rate.value;
 	
 #endif
 	// don't fire underwater
@@ -191,8 +191,8 @@ void CMP5::PrimaryAttack()
 	m_flNextPrimaryAttack = GetNextAttackDelay(0.1);
 
 	if (m_flNextPrimaryAttack < UTIL_WeaponTimeBase())
-		if (sv_mp5_old_rate.value==0) m_flNextPrimaryAttack = UTIL_WeaponTimeBase() + 0.1;
-		if (sv_mp5_old_rate.value == 1) m_flNextPrimaryAttack = UTIL_WeaponTimeBase() + 0.09;
+		if (mp5_old_rate.value==0) m_flNextPrimaryAttack = UTIL_WeaponTimeBase() + 0.1;
+		if (mp5_old_rate.value == 1) m_flNextPrimaryAttack = UTIL_WeaponTimeBase() + 0.09;
 
 	m_flTimeWeaponIdle = UTIL_WeaponTimeBase() + UTIL_SharedRandomFloat(m_pPlayer->random_seed, 10, 15);
 }
